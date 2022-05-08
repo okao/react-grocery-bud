bb82621276c4bbee57e0fd29425a1b6fae6f4e0a
Hamzath Anees <hamzath.anees@ooredoo.mv>
2022-05-08 19:57:44 +0500

    Adding Item adding deleting and alert functionality

--- src/App.js
+++ src/App.js
@@ -10,26 +10,64 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    if (!name) {
+      // setAlert({ show: true, msg: 'Please enter a name', type: 'danger' });
+      showAlert(true, 'danger', 'Please enter a name');
+      return;
+    }else if(name && isEditing) {
+      //deal with edit
+      console.log('edit');
+
+    }else{
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      showAlert(true, 'success', 'Successfully added')
+      // setAlert({ show: true, msg: 'Item added', type: 'success' });
+      setName('');
+    }
   };
+
+  const showAlert = (show=false, type='', msg='') => {
+    setAlert({ show, msg, type });
+  }
+
+  const callDelete = (id) => {
+
+    setList(list.filter((item) => item.id !== id));
+    // setAlert({ show: true, msg: 'Item deleted', type: 'success' });
+
+  }
 
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert type={alert.type} msg={alert.msg} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
 
         <h3>Grocery Bud</h3>
         <div className='form-control'>
-          <input type="text" className='grocery' placeholder='eg. eggs' value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            className='grocery'
+            placeholder='eg. eggs'
+            value={name} onChange={(e) => setName(e.target.value)}
+          />
           <button type='submit' className='submit-btn'>
             {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
-      <div className='grocery-container'>
-        <List />
-        <button className='clear-btn'>Clear Items</button>
-      </div>
+
+      {
+        list.length > 0 &&
+        (
+          <div className='grocery-container'>
+            <List items={list} callDelete={callDelete} />
+            <button className='clear-btn'>Clear Items</button>
+          </div>
+        )
+      }
+
     </section>
   );
 }
